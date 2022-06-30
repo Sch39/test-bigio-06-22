35b91f9f7eed5d020c1c2e926fe47a2b57ef79bf
Sch39 <sch40071807@gmail.com>
2022-06-30 22:47:37 +0700

    update no 20

--- 20/create-basis-data.php
+++ 20/create-basis-data.php
@@ -42,7 +42,7 @@ function query_table(): array
 {
   $querys_init = [
     "CREATE TABLE IF NOT EXISTS `kelas` (
-      `kelas_id` int(11) NOT NULL AUTO_INCREMENT,
+      `kelas_id` INT(11) NOT NULL AUTO_INCREMENT,
       `kelas_nama` varchar(50) DEFAULT NULL,
       PRIMARY KEY (`kelas_id`)
     );",
@@ -99,46 +99,46 @@ function query_table(): array
       PRIMARY KEY (`semester_id`)
     );",
 
-    // "INSERT INTO `semester` (`semester_id`, `semester_nama`) VALUES
-    // (1, 1),
-    // (2, 2);",
+    "INSERT INTO `semester` (`semester_id`, `semester_nama`) VALUES
+    (1, 1),
+    (2, 2);",
 
-    // "CREATE TABLE IF NOT EXISTS `tahun` (
-    //   `tahun_id` int(11) NOT NULL AUTO_INCREMENT,
-    //   `tahun_nama` varchar(20) DEFAULT NULL,
-    //   PRIMARY KEY (`tahun_id`)
-    // );",
+    "CREATE TABLE IF NOT EXISTS `tahun` (
+      `tahun_id` int(11) NOT NULL AUTO_INCREMENT,
+      `tahun_nama` varchar(20) DEFAULT NULL,
+      PRIMARY KEY (`tahun_id`)
+    );",
 
-    // "INSERT INTO `tahun` (`tahun_id`, `tahun_nama`) VALUES
-    // (1, '2014/2015');",
+    "INSERT INTO `tahun` (`tahun_id`, `tahun_nama`) VALUES
+    (1, '2014/2015');",
 
-    // "CREATE TABLE IF NOT EXISTS `users` (
-    //   `id` int(11) NOT NULL AUTO_INCREMENT,
-    //   `nomor_induk` varchar(30) DEFAULT NULL,
-    //   `name` varchar(50) DEFAULT NULL,
-    //   `username` varchar(30) DEFAULT NULL,
-    //   `password` varchar(32) DEFAULT NULL,
-    //   `telp` varchar(16) DEFAULT NULL,
-    //   `alamat` text,
-    //   `status` varchar(25) DEFAULT NULL,
-    //   `jenis_kelamin` varchar(25) DEFAULT NULL,
-    //   `kelas_id` int(11) DEFAULT NULL,
-    //   `access` varchar(30) DEFAULT NULL,
-    //   PRIMARY KEY (`id`),
-    //   KEY `kelas_id` (`kelas_id`),
-    //   KEY `kelas_id_2` (`kelas_id`)
-    // );",
+    "CREATE TABLE IF NOT EXISTS `users` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `nomor_induk` varchar(30) DEFAULT NULL,
+      `name` varchar(50) DEFAULT NULL,
+      `username` varchar(30) DEFAULT NULL,
+      `password` varchar(32) DEFAULT NULL,
+      `telp` varchar(16) DEFAULT NULL,
+      `alamat` text,
+      `status` varchar(25) DEFAULT NULL,
+      `jenis_kelamin` varchar(25) DEFAULT NULL,
+      `kelas_id` int(11) DEFAULT NULL,
+      `access` varchar(30) DEFAULT NULL,
+      PRIMARY KEY (`id`),
+      KEY `kelas_id` (`kelas_id`),
+      KEY `kelas_id_2` (`kelas_id`)
+    );",
 
-    // "INSERT INTO `users` (`id`, `nomor_induk`, `name`, `username`, `password`, `telp`, `alamat`, `status`, `jenis_kelamin`, `kelas_id`, `access`) VALUES
-    // (10, NULL, 'Administrator', 'admin', 'admin', '085761167894', 'Padang', 'PNS', 'Perempuan', NULL, 'admin'),
-    // (25, '56', 'siswa', 'siswa', 'siswa', '456', 'frghf', 'PNS', 'Perempuan', 4, 'siswa'),
-    // (26, 'op', 'guru', 'guru', 'guru', 'op', 'op', 'Honorer', 'Laki-laki', 1, 'guru'),
-    // (27, '1', 'Ary', 'ary', 'ary', '123422', 'asdas', 'dasdas', 'asdas', 2, 'siswa'),
-    // (28, '675675', 'hfgh', 'tgfhf', 'sd', '3453', 'sdfsd', 'Aktif', 'Laki-laki', 1, 'siswa'),
-    // (29, '345', 'fsd', 'sdf', 'sd', '3453', 'sdfsd', 'Aktif', 'Laki-laki', 1, 'siswa'),
-    // (30, '163051100', 'Udin', 'udin', 'udin', '089699996666', 'Cipulang', 'PNS', 'Laki-laki', 6, 'guru'),
-    // (31, '1630', 'Siti', 'siti', 'siti', '0819213', 'iti', 'Aktif', 'Perempuan', 5, 'siswa'),
-    // (32, '1630511', 'nada', 'nada', 'nada', '085255552222', 'Manggarai', 'Aktif', 'Perempuan', 6, 'siswa');",
+    "INSERT INTO `users` (`id`, `nomor_induk`, `name`, `username`, `password`, `telp`, `alamat`, `status`, `jenis_kelamin`, `kelas_id`, `access`) VALUES
+    (10, NULL, 'Administrator', 'admin', 'admin', '085761167894', 'Padang', 'PNS', 'Perempuan', NULL, 'admin'),
+    (25, '56', 'siswa', 'siswa', 'siswa', '456', 'frghf', 'PNS', 'Perempuan', 4, 'siswa'),
+    (26, 'op', 'guru', 'guru', 'guru', 'op', 'op', 'Honorer', 'Laki-laki', 1, 'guru'),
+    (27, '1', 'Ary', 'ary', 'ary', '123422', 'asdas', 'dasdas', 'asdas', 2, 'siswa'),
+    (28, '675675', 'hfgh', 'tgfhf', 'sd', '3453', 'sdfsd', 'Aktif', 'Laki-laki', 1, 'siswa'),
+    (29, '345', 'fsd', 'sdf', 'sd', '3453', 'sdfsd', 'Aktif', 'Laki-laki', 1, 'siswa'),
+    (30, '163051100', 'Udin', 'udin', 'udin', '089699996666', 'Cipulang', 'PNS', 'Laki-laki', 6, 'guru'),
+    (31, '1630', 'Siti', 'siti', 'siti', '0819213', 'iti', 'Aktif', 'Perempuan', 5, 'siswa'),
+    (32, '1630511', 'nada', 'nada', 'nada', '085255552222', 'Manggarai', 'Aktif', 'Perempuan', 6, 'siswa');",
   ];
 
   return $querys_init;
